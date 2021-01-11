48788119910e4c8bc97c8205f88d78a18de7ff0b
Brian Evans <ebrian101@gmail.com>
2021-01-11 22:38:30 +0000

    Got connection to database working

--- basic-csv-to-db.js
+++ basic-csv-to-db.js
@@ -2,14 +2,23 @@
 const readJson = async(filename) => {
     const csv = require('csv-parser')
     const fs = require('fs')
-
+//	console.log("CA CERT: ")
+//console.log(process.env.CA_CERT)
     const {Client} = require("pg") ;
-    const client = new Client();
+    const client = new Client({
+//	database: process.env.PGDATABASE,
+//	user: process.env.PGUSER,
+	    ssl: {
+		    rejectUnauthorized: false,
+//		    ca:process.env.CA_CERT
+			require: true
+	    }
+    });
     try{
     await client.connect()
     client.on("error", console.log);
 
-    const sqlQuery = "INSERT INTO companies (name, number, streetAddress, county, country, postCode, category, status, origin, date, SicCode1, SicCode2, SicCode3, SicCode4) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) ON CONFLICT (number) DO NOTHING;"
+    const sqlQuery = "INSERT INTO companies (name, number, streetAddress, county, country, postCode, category, status, origin, date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, TO_TIMESTAMP($10, 'DD/MM/YYYY')) ON CONFLICT (number) DO NOTHING;"
 
     const companies = []
     console.time('Parse data')
@@ -17,8 +26,8 @@ const readJson = async(filename) => {
       .pipe(csv({
           mapHeaders: ({header, index})=> headerMapper[header.trim()]||null
       }))
-      .on('data', (data) => {
-          client.query(sqlQuery, Object.values(data), (err, res)=>{
+      .on('data', async(data) => {
+          await client.query(sqlQuery, Object.values(data).slice(0,10), (err, res)=>{
               if(err) console.log(err)
               else companies.push(data)
           })
@@ -28,14 +37,20 @@ const readJson = async(filename) => {
         // console.log(companies);
         console.timeEnd('Parse data')
           console.log(`Uploaded ${companies.length} companies`)
-        // [
-        //   { NAME: 'Daffy Duck', AGE: '24' },
-        //   { NAME: 'Bugs Bunny', AGE: '22' }
-        // ]
+//        client.end()
+//	      console.log("Connection with database ended")
       });
     }catch (e) {
         console.log(e)
+	    
+    }finally
+	{
+		console.log("Finally")
     }
+	setTimeout(()=>{
+		client.end()
+		console.log("Ended connected to db")
+	}, 1000)
 }
 
 readJson('basic-sample.csv')
