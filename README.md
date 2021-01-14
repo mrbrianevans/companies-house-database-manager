0690b5b19aad4cbf3b9201b385074fa609aed500
Brian Evans <ebrian101@gmail.com>
2021-01-14 14:44:38 +0000

    Added progress viewer to basic data uploader

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# companies-house-database-manager
-Keeps the database up to date from a virtual machine
+ # What is this unit for?
+
+This compute unit / droplet is to load the database.
+
+This should have a cron job to fetch data from Companies House and ingest it and save it in PostGreSQL Database.
+
+Then the backend hosted on App Platform under companies-house-frontend will serve the database data to the frontend through nextJS.
+
+The frontend will be a way to interact with it.
--- basic-csv-to-db.js
+++ basic-csv-to-db.js
@@ -18,48 +18,57 @@ const readJson = async(filename) => {
     await client.connect()
     client.on("error", console.log);
 
-    const companyQuery = "INSERT INTO companies (name, number, streetAddress, county, country, postCode, " +
-        "category, status, origin, date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, " +
-        "TO_TIMESTAMP($10, 'DD/MM/YYYY')) ON CONFLICT (number) DO NOTHING;"
+    const companyQuery = "INSERT INTO companies (name, number, streetAddress, county, country, postCode, category, status, origin, date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, TO_TIMESTAMP($10, 'DD/MM/YYYY')) ON CONFLICT (number) DO NOTHING;"
     let sic_errors = 0
-    const sicQuery = "INSERT INTO sic (company_number, sic_code) VALUES ($1, $2);"
+    const sicQuery = "INSERT INTO sic (company_number, sic_code) VALUES ($1, $2) ON CONFLICT (company_number, sic_code) DO NOTHING;"
     let companies = 0
+	    let company_errors = 0
     console.time('Parse data')
-    const fileReadStream = fs.createReadStream(filename)
+const startTime = Date.now()
+const progressUpdater =  setInterval(()=>{
+		// report on progress
+		 process.stdout.clearLine()
+		    process.stdout.cursorTo(0)
+	let timeTaken = Date.now()-startTime
+	let message = `${Math.floor(timeTaken/1000)}s Progress: \x1b[32m${companies} OK\x1b[0m companies, \x1b[31m${sic_errors} ERR\x1b[0m sics, \x1b[31m${company_errors} ERR\x1b[0m companies, AVERAGE SPEED: \x1b[36m${Math.round(companies/timeTaken*1000)/1000}ms\x1b[0m per company`
+		    process.stdout.write(message)
+	    }, 1000)
+const fileReadStream = fs.createReadStream(filename)
       .pipe(csv({
           mapHeaders: ({header, index})=> headerMapper[header.trim()]||null
       }))
       .on('data', async(data) => {
           fileReadStream.pause() // pause reading while database upload takes place
           await client.query(companyQuery, Object.values(data).slice(0,10))
-              .then(async(err, res)=>{
-                  if(err) console.log(err)
-                  else {
+              .then(async(res)=>{
+                  
                       const sicOne = data.SicCode1.match(/^[0-9]{5}/)
                       if(sicOne)
-                        await client.query(sicQuery, [data.number, sicOne[0]]).then(err=>{if(err) sic_errors++})
+                        await client.query(sicQuery, [data.number, sicOne[0]]).catch(err=>{if(err) sic_errors++})
 
                       const sicTwo = data.SicCode2.match(/^[0-9]{5}/)
                       if(sicTwo)
-                        await client.query(sicQuery, [data.number, sicTwo[0]]).then(err=>{if(err) sic_errors++})
+                        await client.query(sicQuery, [data.number, sicTwo[0]]).catch(err=>{if(err) sic_errors++})
 
                       const sicThree = data.SicCode3.match(/^[0-9]{5}/)
                       if(sicThree)
-                        await client.query(sicQuery, [data.number, sicThree[0]]).then(err=>{if(err) sic_errors++})
+                        await client.query(sicQuery, [data.number, sicThree[0]]).catch(err=>{if(err) sic_errors++})
 
                       const sicFour = data.SicCode4.match(/^[0-9]{5}/)
                       if(sicFour)
-                        await client.query(sicQuery, [data.number, sicFour[0]]).then(err=>{if(err) sic_errors++})
+                        await client.query(sicQuery, [data.number, sicFour[0]]).catch(err=>{if(err) sic_errors++})
 
                       companies++
-                  }
-      //    console.timeLog('Parse data', `${companies} companies parsed`)
-          })
+                  
+//     	    console.timeLog('Parse data', `${companies} companies parsed`)
+          }).catch(err=>company_errors++)
           fileReadStream.resume()
       })
       .on('end', () => {
         // console.log(companies);
+	      clearInterval(progressUpdater)
         console.timeEnd('Parse data')
+	      console.log(`${sic_errors} errors with inserting SIC codes`)
           console.log(`Uploaded ${companies} companies`)
        client.end()
 	      console.log("Connection with database ended")
@@ -67,9 +76,6 @@ const readJson = async(filename) => {
     }catch (e) {
         console.log(e)
 
-    }finally
-	{
-		console.log("Finally")
     }
 	// setTimeout(()=>{
 	// 	client.end()
@@ -77,8 +83,10 @@ const readJson = async(filename) => {
 	// 	console.timeEnd("Parse data")
 	// }, 60*60*1000)//disconnect after 1 hour
 }
-const path = require('path')
-await readJson(path.resolve(__dirname, process.argv[2]))
+(async () => {
+	const path = require('path')
+	await readJson(path.resolve(__dirname, process.argv[2]))
+})()
 
 const headerMapper = {
     CompanyName: 'name',
