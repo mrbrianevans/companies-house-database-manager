511c5e7cbf9d4663401ed3138ecd741ab4aad6d6
Brian Evans <ebrian101@gmail.com>
2021-04-28 09:34:14 +0100

    Filing Events save to DB improvements
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- events/saveFilingEventsCloudFunction/processFilingEvent.ts
+++ events/saveFilingEventsCloudFunction/processFilingEvent.ts
@@ -12,10 +12,17 @@ export const processFilingEvent = async (filingEvent: FilingEvent.FilingEvent, p
     if (rowCount !== 1) return // can't find description in database (rare)
     const description = descriptions[0]['value']
     let formattedDescription = description.replace(/{([a-z_]+)}/g, (s) => filingEvent.data.description_values ? filingEvent.data.description_values[s.slice(1, s.length - 1)] || '' : '')
-    formattedDescription = formattedDescription.replace(/^\*\*/, '<b>')
-    formattedDescription = formattedDescription.replace(/\*\*/, '</b>')
+    // VERY BAD IDEA TO PUT HTML INTO THE DATABASE!!
+    // formattedDescription = formattedDescription.replace(/^\*\*/, '<b>')
+    // formattedDescription = formattedDescription.replace(/\*\*/, '</b>')
     // SAVE EVENT TO DATABASE
     // it seems to be getting each event twice, which causes a problem for the primary key when it tries to insert the same row again. thats why there is on conflict do nothing. this is a short term fix
-    let insertParameters = [filingEvent.resource_id, filingEvent.data.category, filingEvent.data.description, formattedDescription, filingEvent.data.date, filingEvent.event.timepoint, filingEvent.event.published_at, filingEvent.data.barcode, filingEvent.data.type, companyNumber]
-    await pool.query("INSERT INTO filing_events (id, category, description_code, description, filing_date, timepoint, published, barcode, type, company_number) VALUES ($1,$2,$3, $4, $5, $6, $7, $8, $9, $10) ON CONFLICT DO NOTHING;", insertParameters)
+    // duplicates have been fixed by making id the primary key
+    let insertParameters = [filingEvent.resource_id, filingEvent.data.category, filingEvent.data.description, filingEvent.data.description_values, formattedDescription, filingEvent.data.date, filingEvent.event.timepoint, filingEvent.event.published_at, filingEvent.data.barcode, filingEvent.data.type, companyNumber]
+    await pool.query(`
+        INSERT INTO filing_events
+        (id, category, description_code, description_values, description, filing_date, timepoint,
+         published, barcode, type, company_number)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+        ON CONFLICT DO NOTHING;`, insertParameters)
 }
