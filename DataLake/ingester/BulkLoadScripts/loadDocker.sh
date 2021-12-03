451d2f55b842503b079a72e019fa1c9869e11848
Brian Evans <ebrian101@gmail.com>
2021-12-03 10:29:19 +0000

    Setup PM2 to manage processes and cron jobs
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- DataLake/ingester/BulkLoadScripts/loadDocker.sh
+++ DataLake/ingester/BulkLoadScripts/loadDocker.sh
@@ -8,19 +8,16 @@ name=$2
 file_ext=$3
 url=$4
 
-container_name="docker-dl-1"
-# create volume if not exists
-docker volume create DataSourceFiles
 # copy file to volume accessible to mongo container
-docker cp "$tmp_filename" "$container_name:/DataSourceFiles/$name.$file_ext"
+mv "$tmp_filename" "/BulkFiles/$name.$file_ext"
 
 # requires environment variables for authentication (~/data-manager/docker/.mongo.env)
 
 # run mongo import on that copied file
-time docker exec "$container_name" mongoimport --db=lake --drop --file "/DataSourceFiles/$name.$file_ext" --type="$file_ext" \
+mongoimport --db=lake --drop --file "/BulkFiles/$name.$file_ext" --type="$file_ext" \
  --username="$MONGO_INITDB_ROOT_USERNAME" --password="$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin \
  $([ "$file_ext" == "csv" ] && echo "--headerline") --numInsertionWorkers=2
 
 # move to cloud
-echo "Moving $tmp_filename file to Google Cloud"
-time gsutil -h "x-goog-meta-data-source-url:$url" mv "$tmp_filename" "gs://companies-house-data-sources/$name.$file_ext"
+echo "Moving /BulkFiles/$name.$file_ext file to Google Cloud"
+time gsutil -h "x-goog-meta-data-source-url:$url" mv "/BulkFiles/$name.$file_ext" "gs://companies-house-data-sources/$name.$file_ext"
