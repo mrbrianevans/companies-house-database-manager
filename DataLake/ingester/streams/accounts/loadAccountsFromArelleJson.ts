fb925d4fb7b2ac570b55876446dbd2855260e99a
Brian Evans <ebrian101@gmail.com>
2022-02-16 12:07:56 +0000

    Save transformed accounts ready for loading into mongo
    Also made ingester/streams an ES module
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- DataLake/ingester/streams/accounts/loadAccountsFromArelleJson.ts
+++ DataLake/ingester/streams/accounts/loadAccountsFromArelleJson.ts
@@ -7,6 +7,7 @@ import {getPostgresPool} from "../getPostgresPool";
 
 // directory containing *only* JSON files from Arelle
 const ROOT_DIR = 'N:\\CompaniesHouse\\Accounts\\JSON'
+const OUT_DIR = 'N:\\CompaniesHouse\\Accounts\\MongoAccounts'
 
 async function loadAccountsFromArelleJson() {
 
@@ -21,9 +22,9 @@ async function loadAccountsFromArelleJson() {
         return {done: true, value: null}
       }
       const filename = fileList[fileCounter++]
-      console.time('Get file')
+      // console.time('Get file')
       const facts = await getFile(filename)
-      console.timeEnd('Get file')
+      // console.timeEnd('Get file')
       const value = {facts, filename}
       return {value, done: false}
     }
@@ -34,7 +35,7 @@ async function loadAccountsFromArelleJson() {
   for await(const {facts, filename} of fileIterable) {
     const mongoFacts = convertArelleToMongoAccounts(facts, dimensions)
     const mongoDoc = await getMongoAccounts(mongoFacts, filename)
-    // console.log(mongoDoc.facts.filter(f=>f.additionalInfo))
+    await saveMongoAccounts(mongoDoc)
   }
 }
 
@@ -47,7 +48,8 @@ async function getDimensions(): Promise<Map<string, string>> {
 
 async function getListOfFiles(): Promise<string[]> {
   const files = await fs.readdir(ROOT_DIR, {withFileTypes: true})
-  return files.filter(f => f.isFile()).filter(f => f.name).map(f => f.name).slice(0, 100)
+  const doneFiles = new Set(await fs.readdir(OUT_DIR))
+  return files.filter(f => f.isFile()).filter(f => f.name).filter(f => !doneFiles.has(f.name)).map(f => f.name).slice(0, 10000)
 }
 
 async function getFile(filepath: string) {
@@ -71,7 +73,8 @@ function renameDimensions(mapper: Map<string, string>, obj?: Record<string, stri
     // COUNT NOT REPLACE () for KEY: char:CharityFundsDimension CharityFundsDimension undefined undefined
     // COULD NOT FIND VALUE: char:TotalRestrictedIncomeFunds
     // .
-    // the char namespace for charities
+    // the char namespace for charities, also the ns22 namespace for charities
+    // see 00157327_2020-12-31 as an example. results in things like Trustee6 with no space
     return [newKey?.[1], newValue ?? value.replace(/^.*:/, '')]
   }))
 }
@@ -79,17 +82,16 @@ function renameDimensions(mapper: Map<string, string>, obj?: Record<string, stri
 function convertArelleToMongoAccounts(arelle: ArelleJsonAccounts, dimensions: Map<string, string>): MongoAccounts['facts'] {
   return arelle.factList.map(fact => {
     const newFact: MongoAccounts['facts'][number] = {
-      label: fact[2].label,
-      value: fact[2].value,
-      endDate: fact[2].endInstant,
-      startDate: fact[2].start
+      label: fact[2].label, value: fact[2].value, endDate: fact[2].endInstant, startDate: fact[2].start
     }
     const factDimensions = renameDimensions(dimensions, fact[2].dimensions)
     const info = getOnlyValueOfObject(factDimensions)
     if (info) {
-      if (newFact.value === '(reported)') newFact.value = info
-      else if (Object.keys(factDimensions ?? {}).length === 1) newFact.info = info
-      else newFact.additionalInfo = factDimensions
+      // i decided it would be better to do this kind of processing on the frontend, as it results in information loss.
+      // if (newFact.value === '(reported)') newFact.value = info
+      // else if (Object.keys(factDimensions ?? {}).length === 1) newFact.info = info
+      // else
+      newFact.additionalInfo = factDimensions
     }
 
     return newFact
@@ -105,11 +107,7 @@ async function getMongoAccounts(facts: MongoAccounts['facts'], filename: string)
   const [, companyNumber, accountsDate] = filenameParts
   const filingEvent = await getFilingEvent(companyNumber, accountsDate)
   return {
-    ...filingEvent,
-    accountsDate,
-    companyNumber,
-    facts,
-    numberOfFacts: facts.length,
+    ...filingEvent, accountsDate, companyNumber, facts, numberOfFacts: facts.length,
   }
 }
 
@@ -120,23 +118,30 @@ async function getFilingEvent(companyNumber: string, accountsDate: string) {
   //todo: should only load filing events into postgres if the one being looked for doesn't already exist
   await loadFilingEventsIntoPostgres(companyNumber, pool)
   const filingEvent = await pool.query(`
-                      SELECT id                 AS _id,
-                             description_code   AS "descriptionCode",
-                             description,
-                             description_values AS "descriptionValues",
-                             filing_date::text  AS "filingDate",
-                             barcode,
-                             type
-                      FROM filing_events
-                      WHERE company_number = $1
-                        AND description_values ->> 'made_up_date' = $2
-      `,
-      [companyNumber, accountsDate])
+              SELECT id                 AS _id,
+                     description_code   AS "descriptionCode",
+                     description,
+                     description_values AS "descriptionValues",
+                     filing_date::text  AS "filingDate",
+                     barcode,
+                     type
+              FROM filing_events
+              WHERE company_number = $1
+                AND description_values ->> 'made_up_date' = $2
+    `, [companyNumber, accountsDate])
     .then(res => res.rows[0] as { _id: string, descriptionCode: string, description: string, descriptionValues: { made_up_date: string }, filingDate: string, barcode: string, type: string })
   await pool.end()
   if (!filingEvent) throw new Error(`Cant find filing event for companyNumber=${companyNumber} and accountsDate=${accountsDate}`)
   return filingEvent
 }
 
+async function saveMongoAccounts(accounts: MongoAccounts) {
+  await fs.writeFile(path.resolve(OUT_DIR, `${accounts.companyNumber}_${accounts.accountsDate}.json`), JSON.stringify(accounts, null, 2))
+}
+
 // run main function
-loadAccountsFromArelleJson()
+await loadAccountsFromArelleJson()
+
+
+
+
